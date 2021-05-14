559ccfb13dbbfd77e7dda4a5f8d0c8e32a2f0652
MARDAScience <dbuscombe@gmail.com>
2021-05-14 16:25:12 -0700

    Deploy website - based on fce3270a6b703b806209cc6ea05866640c0842b8

---
+++ assets/js/a7023ddc.089c40a3.js
@@ -0,0 +1,1 @@
+(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[713],{7485:function(s){"use strict";s.exports=JSON.parse('{"web":{"allTagsPath":"/blog/tags","slug":"web","name":"web","count":1,"permalink":"/blog/tags/web"},"scicomm":{"allTagsPath":"/blog/tags","slug":"scicomm","name":"scicomm","count":1,"permalink":"/blog/tags/scicomm"}}')}}]);

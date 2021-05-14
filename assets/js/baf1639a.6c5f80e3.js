9d65af91bdce647c16105feb96e6b0dcb0999c2c
MARDAScience <dbuscombe@gmail.com>
2021-05-13 21:30:14 -0700

    Deploy website - based on d85de65fcc3d98ad9513ffc27fe8dafbe747f8cf

--- assets/js/baf1639a.6c5f80e3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[520],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/web/blog/welcome"},{"title":"Hello","permalink":"/web/blog/hello-world"},{"title":"Hola","permalink":"/web/blog/hola"}]}')}}]);

128d10bdb5fb8575fea9331cd0e7a7f29ae68b02
MARDAScience <dbuscombe@gmail.com>
2021-05-14 09:08:02 -0700

    Deploy website - based on d17acb974fd514b365b4813ee6b0cbbf4d80641b

--- assets/js/ed105bb0.b744c750.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[615],{5430:function(s){"use strict";s.exports=JSON.parse('{"allTagsPath":"/web/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/web/blog/tags/docusaurus"}')}}]);
--- assets/js/f01b3634.543589a7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[889],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/web/blog/welcome"},{"title":"Hello","permalink":"/web/blog/hello-world"},{"title":"Hola","permalink":"/web/blog/hola"}]}')}}]);
